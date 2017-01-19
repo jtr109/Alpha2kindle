a8c2ddf851efefb2ce691060f77aa4fc138b9849
jtr109 <lyp_login@outlook.com>
2017-01-19 21:19:34 +0800

    test send mail

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,10 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import configparser
+
+
+def parse_config(key):
+    config = configparser.ConfigParser()
+    build_config = 'build.ini'
+    config.read(build_config)
+    return config[key]  # raise error if the key not exist in config file
--- utils/mail_sender.py
+++ utils/mail_sender.py
@@ -1,12 +1,42 @@
 # -*- coding: utf-8 -*-
 
-import os
-import configparser
+from email import encoders
+from email.header import Header
+from email.mime.text import MIMEText
+from email.utils import parseaddr, formataddr
+import smtplib
 
+from utils import parse_config
+
+
+class MailSender(object):
+
+    def __init__(self):
+        mail_sender = parse_config('mail_sender')
+        self.from_addr = mail_sender['username']
+        self.password = mail_sender['pwd']
+        self.smtp_server = mail_sender['smtp_server']
+        self.ssl_port = mail_sender['ssl_port']
+
+    def _format_addr(self, s):
+        name, addr = parseaddr(s)
+        return formataddr((Header(name, 'utf-8').encode(), addr))
+
+    def send_mail(self):
+        to_addr = input('To: ')
+
+        msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+        msg['From'] = self._format_addr('Python爱好者 <%s>' % self.from_addr)
+        msg['To'] = self._format_addr('管理员 <%s>' % to_addr)
+        msg['Subject'] = Header('来自SMTP的问候……', 'utf-8').encode()
+
+        server = smtplib.SMTP_SSL(self.smtp_server, self.ssl_port)
+        server.set_debuglevel(1)
+        server.login(self.from_addr, self.password)
+        server.sendmail(self.from_addr, [to_addr], msg.as_string())
+        server.quit()
 
 
 if __name__ == '__main__':
-    config = configparser.ConfigParser()
-    config.read('build.ini')
-    mail_sender = config['mail_sender']
-    print(mail_sender['username'])
+    mail_sender = MailSender()
+    mail_sender.send_mail()
